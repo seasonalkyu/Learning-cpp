e791259a3db8af7f68fa7c23106a17a3e34309b3
seasonalkyu <jechulchankyu@gmail.com>
2022-07-18 19:54:10 +0900

    7/18

--- Chapter12/Ch12_07/소스.cpp
+++ Chapter12/Ch12_07/소스.cpp
@@ -15,11 +15,14 @@ public:
 public:
 	string getName() { return m_name; }
 
-	virtual void speak() const
-		//void speak() const
+	// pure virtual function 선언
+	virtual void speak() const = 0;
+	/*void speak() const
 	{
 		cout << m_name << " ??? " << endl;
 	}
+	*/
+	// body를 없앤다 
 };
 
 class Cat : public Animal
@@ -48,49 +51,27 @@ public:
 	}
 };
 
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Woof " << endl;
+	}
+};
+
 int main()
 {
-	Animal animal("my animal");
-	Cat cat("my cat");
-	Dog dog("my dog");
-
-	animal.speak();
-	cat.speak();
-	dog.speak();
-
-	Animal* ptr_animal1 = &cat;
-	Animal* ptr_animal2 = &dog;
-
-	ptr_animal1->speak();
-	ptr_animal2->speak();
-	// dog class이지만 부모클래스인 Animal로 인식하고 작동
-
-	Cat cats[] = { Cat("cat1"), Cat("cat2"), Cat("cat3") };
-	Dog dogs[] = { Dog("dog1"), Dog("Dog2") };
-
-	for (int i = 0; i < 3; ++i)
-		cats[i].speak();
-
-	for (int i = 0; i < 2; ++i)
-		dogs[i].speak();
-
-	Animal* my_animals[] = { &cats[0], &cats[1], &cats[3], &dogs[0], &dogs[1] };
-	for (int i = 0; i < 5; ++i)
-		my_animals[i]->speak();
-
+	//Animal animal("my animal");
+		// error가 난다
 
 	return 0;
 }
 
-
-/*
-* 자식 클래스의 객체에 부모 클래스의 포인터를 사용한다면?
-* => 다형성과 관련된 질문
-*
-* 부모의 함수에 virtual 키워드를 넣으면 부모 클래스의 포인터를 이용해서 자식 클래스를 가리키더라도
-* 부모 클래스의 함수가 아닌 자식 클래스의 오버로딩된 함수를 이용한다.
-* 이러한 성질을 다형성이라고 한다
-*/
 /*
 * 순수 가상 함수: 
 *	바디가 없다. 따라서 반드시 자식 클래스에서 오버라이드해야한다
